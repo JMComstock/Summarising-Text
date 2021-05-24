f1d615761bf9fb45b9d7b3a6cb3d1e5221ee8eb2
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-24 12:28:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarising_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really long, actually to ridicliously long text.";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }  
+    }
+}
